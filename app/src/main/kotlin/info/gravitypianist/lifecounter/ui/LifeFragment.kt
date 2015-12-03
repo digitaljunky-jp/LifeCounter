ec7a2f2777dd2a1002f80e042024a2d80e5ef8c1
hyata <yata@team-lab.com>
2015-12-03 10:01:24 +0900

    カウントできるようにした

---
+++ app/src/main/kotlin/info/gravitypianist/lifecounter/ui/LifeFragment.kt
@@ -0,0 +1,19 @@
+package info.gravitypianist.lifecounter.ui
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import info.gravitypianist.lifecounter.R
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+class LifeFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater!!.inflate(R.layout.fragment_life, container, false)
+    }
+}
--- app/src/main/kotlin/info/gravitypianist/lifecounter/view/LifeView.kt
+++ app/src/main/kotlin/info/gravitypianist/lifecounter/view/LifeView.kt
@@ -2,21 +2,51 @@ package info.gravitypianist.lifecounter.view
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.Button
 import android.widget.RelativeLayout
+import android.widget.TextView
+import butterknife.bindView
 import info.gravitypianist.lifecounter.R
+import java.util.logging.Logger
 
 /**
 
  * Created by hyata on 2015/11/19.
  */
 class LifeView(context: Context, attrs: AttributeSet) : RelativeLayout(context, attrs) {
+
+    val valueText : TextView by bindView(R.id.value_text)
+
+    val plus1Button : Button by bindView(R.id.plus_1_button)
+
+    val minus1Button : Button by bindView(R.id.minus_1_button)
+
+    val plus5Button : Button by bindView(R.id.plus_5_button)
+
+    val minus5Button : Button by bindView(R.id.minus_5_button)
+
+
+    var value:Int = 0
+    set(n){
+        field = n
+        valueText.text = field.toString()
+    }
+
     init {
         val array = context.obtainStyledAttributes(attrs, R.styleable.LifeView)
         val type = array.getInt(R.styleable.LifeView_type, 0)
-        val layout = inflateLayout(context, type, this)
+        inflateLayout(context, type, this)
+        value = array.getInt(R.styleable.LifeView_value, 0)
         array.recycle()
+
+        plus1Button.setOnClickListener { view -> value += 1 }
+        plus5Button.setOnClickListener { view -> value += 5 }
+
+        minus1Button.setOnClickListener { view -> value -= 1 }
+        minus5Button.setOnClickListener { view -> value -= 5 }
     }
 
     private fun inflateLayout(context: Context, type: Int, layout: RelativeLayout): View {
